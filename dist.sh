c07639f741fe022690afb4b46c69d19679b7e38d
Rick Roberts <web.ayso@gmail.com>
2016-09-28 08:39:20 -0700

    Update distribution script

--- dist.sh
+++ dist.sh
@@ -13,7 +13,7 @@ cp -f -r app dist/app
 cp -f -r vendor dist/vendor
 cp -f -r public dist/public
 cp -f -r templates dist/templates
-
+cp -f -r config dist/config
 cp -f -r src/Action dist/src
 
 find dist -type f -name '.DS_Store' -delete
