e10d730b2097726ff4c3b49662d0840e1384cb83
Rick Roberts <web.ayso@gmail.com>
2016-09-30 12:21:43 -0700

    Updated list script to apply app_prod

--- dist.sh
+++ dist.sh
@@ -12,8 +12,10 @@ mkdir dist/src
 cp -f -r app dist/app
 cp -f -r vendor dist/vendor
 cp -f -r public dist/public
+mv -f dist/public/app_prod.php dist/public/app.php
 cp -f -r templates dist/templates
 cp -f -r config dist/config
 cp -f -r src/Action dist/src
 
 find dist -type f -name '.DS_Store' -delete
+find dist -type f -name 'app_*' -delete
