27a60c705724da1e8af1adcb8181659f061f1291
Rick Roberts <web.ayso@gmail.com>
2016-10-26 17:17:20 -0700

    Add file to create database & tables without data

---
+++ sql/20161025.schema.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 27, 2016 at 12:12 AM
+-- Server version: 5.6.33-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wp_ayso1ref`
+--
+CREATE DATABASE IF NOT EXISTS `wp_ayso1ref` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `wp_ayso1ref`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rs_events`
+--
+
+DROP TABLE IF EXISTS `rs_events`;
+CREATE TABLE `rs_events` (
+  `id` int(11) NOT NULL,
+  `projectKey` varchar(45) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `dates` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `locked` tinyint(1) DEFAULT '1',
+  `view` tinyint(1) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `num_refs` int(11) DEFAULT '3',
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rs_games`
+--
+
+DROP TABLE IF EXISTS `rs_games`;
+CREATE TABLE `rs_games` (
+  `id` int(11) NOT NULL,
+  `projectKey` varchar(45) NOT NULL,
+  `game_number` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `field` varchar(45) DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `division` varchar(45) DEFAULT NULL,
+  `pool` varchar(3) DEFAULT NULL,
+  `home` varchar(45) DEFAULT NULL,
+  `away` varchar(45) DEFAULT NULL,
+  `assignor` varchar(45) DEFAULT NULL,
+  `cr` varchar(45) DEFAULT NULL,
+  `ar1` varchar(45) DEFAULT NULL,
+  `ar2` varchar(45) DEFAULT NULL,
+  `r4th` varchar(45) DEFAULT NULL,
+  `medalRound` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rs_limits`
+--
+
+DROP TABLE IF EXISTS `rs_limits`;
+CREATE TABLE `rs_limits` (
+  `id` int(11) NOT NULL,
+  `projectKey` varchar(45) NOT NULL,
+  `division` varchar(10) NOT NULL,
+  `limit` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rs_users`
+--
+
+DROP TABLE IF EXISTS `rs_users`;
+CREATE TABLE `rs_users` (
+  `id` int(11) NOT NULL,
+  `name` char(255) NOT NULL,
+  `enabled` tinyint(1) DEFAULT '1',
+  `hash` varchar(255) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `active_event_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rs_events`
+--
+ALTER TABLE `rs_events`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- Indexes for table `rs_games`
+--
+ALTER TABLE `rs_games`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- Indexes for table `rs_limits`
+--
+ALTER TABLE `rs_limits`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- Indexes for table `rs_users`
+--
+ALTER TABLE `rs_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `rs_events`
+--
+ALTER TABLE `rs_events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `rs_games`
+--
+ALTER TABLE `rs_games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=521;
+--
+-- AUTO_INCREMENT for table `rs_limits`
+--
+ALTER TABLE `rs_limits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+--
+-- AUTO_INCREMENT for table `rs_users`
+--
+ALTER TABLE `rs_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
